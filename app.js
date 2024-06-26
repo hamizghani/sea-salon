37ce2d23a66d342be35a056783fd4b068f09b6d7
hamizghani <hamizghaniiii@gmail.com>
2024-06-26 23:49:09 +0700

    fix: responsive nih

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 $('#brand > h1').on('click',function() {
     window.location.href = 'testsea.html';
 });
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const stars = document.querySelectorAll('.star');
+
+    stars.forEach(star => {
+        star.addEventListener('click', () => {
+            stars.forEach(s => s.classList.remove('selected'));
+            star.classList.add('selected');
+            let ratingValue = star.getAttribute('data-value');
+            console.log(`Rating: ${ratingValue}`);
+        });
+    });
+});
